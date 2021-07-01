b003c5246c2fd14d09383166c4f3fa8dfed66d4e
Jernej Jakob <jernej.jakob@gmail.com>
2021-07-01 15:03:44 +0200

    Change OSCCAL in small steps
    The datasheet says OSCCAL should not be changed more than 0x20 at a
    time. To do this, compute the difference between wanted and current
    OSCCAL and increment/decrement it in 0xf (16)-sized steps with 10us
    delays in between.

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 #define	LED_RED		PB0
 #define	LED_GREEN	PB1
 #define IN_PWROK_MUTE	PB2
 #define IN_PROT_TEMP	PB3
 #define	RELAY		PB4
+
+#define OSCCAL_STEP 16
 
 int main(void) {
 	/* set both LEDs on at start for diagnostics */
@@ -17,8 +20,23 @@ int main(void) {
 	DDRB = _BV(LED_RED)|_BV(LED_GREEN)|_BV(RELAY);
 
 #ifdef PROG_CAL
-	/* load osccal */
-	OSCCAL = PROG_CAL;
+	/* load osccal, in small steps */
+	int8_t osccal_diff = (int8_t)(PROG_CAL - OSCCAL);
+	uint8_t i;
+	if(osccal_diff > 0) {
+		for(i=osccal_diff/OSCCAL_STEP;i>0;i--){
+			OSCCAL += OSCCAL_STEP;
+			_delay_us(10);
+		}
+		OSCCAL += osccal_diff%OSCCAL_STEP;
+	}
+	else if(osccal_diff < 0) {
+		for(i=abs(osccal_diff)/OSCCAL_STEP;i>0;i--){
+			OSCCAL -= OSCCAL_STEP;
+			_delay_us(10);
+		}
+		OSCCAL -= abs(osccal_diff)%OSCCAL_STEP;
+	}
 #endif
 
 	/* turn off leds */
