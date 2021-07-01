7946f87fe51242053e752e151e18bcc78c98a77e
Jernej Jakob <jernej.jakob@gmail.com>
2021-07-01 16:38:33 +0200

    Relay output, delay and LED blinking

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #define IN_PWRFAIL_MUTE_ADMUX 1
 #define IN_PROT_TEMP_ADMUX 2
 
+/* thresholds and hysteresis are 8-bit ADC values */
 #define MUTE_THRESHOLD 128
 #define PWRFAIL_THRESHOLD 10
 
@@ -22,6 +23,9 @@
 #define PROT_THRESHOLD 10
 
 #define ADC_HYSTERESIS 10
+
+/* produces about 500ms delay */
+#define MEDIUM_DELAY 65535
 
 #define OSCCAL_STEP 16
 
@@ -46,6 +50,9 @@ int main(void) {
 		STATE_PROT,
 		STATE_NORMAL,
 	} __attribute__((packed)) state = STATE_START;
+
+	/* main loop must not use blocking delays, so we delay by incrementing this variable */
+	uint16_t delayvar = 0;
 
 	/* set both LEDs on at start for diagnostics */
 	/* IN_PWRFAIL_MUTE needs internal pullup */
@@ -94,20 +101,32 @@ int main(void) {
 			intflags.adc_int = 0;
 
 			if((ADMUX & (MUX0|MUX1)) == IN_PWRFAIL_MUTE_ADMUX){
-				if(adcval >= MUTE_THRESHOLD)
+				if(adcval >= MUTE_THRESHOLD && state != STATE_MUTE) {
 					state = STATE_MUTE;
-				else if((MUTE_THRESHOLD-ADC_HYSTERESIS > adcval) && (adcval > PWRFAIL_THRESHOLD+ADC_HYSTERESIS))
+					delayvar = 0;
+				}
+				else if(MUTE_THRESHOLD-ADC_HYSTERESIS > adcval && adcval > PWRFAIL_THRESHOLD+ADC_HYSTERESIS && state != STATE_PWRFAIL) {
 					state = STATE_PWRFAIL;
-				else if((adcval <= PWRFAIL_THRESHOLD) && ((state == STATE_MUTE)||(state == STATE_PWRFAIL)))
+					delayvar = 0;
+				}
+				else if(adcval <= PWRFAIL_THRESHOLD && (state == STATE_MUTE || state == STATE_PWRFAIL)) {
 					state = STATE_NORMAL;
+					delayvar = 0;
+				}
 			}
 			else {
-				if((adcval >= TEMP_THRESHOLD) && ((state == STATE_TEMP)||(state == STATE_PROT)))
+				if(adcval >= TEMP_THRESHOLD && (state == STATE_TEMP || state == STATE_PROT)) {
 					state = STATE_NORMAL;
-				else if((TEMP_THRESHOLD-ADC_HYSTERESIS > adcval) && (adcval > PROT_THRESHOLD+ADC_HYSTERESIS))
+					delayvar = 0;
+				}
+				else if(TEMP_THRESHOLD-ADC_HYSTERESIS > adcval && adcval > PROT_THRESHOLD+ADC_HYSTERESIS && state != STATE_TEMP) {
 					state = STATE_TEMP;
-				else if(adcval <= PROT_THRESHOLD)
+					delayvar = 0;
+				}
+				else if(adcval <= PROT_THRESHOLD && state != STATE_PROT) {
 					state = STATE_PROT;
+					delayvar = 0;
+				}
 			}
 
 			/* alternate between the 2 inputs */
@@ -115,9 +134,11 @@ int main(void) {
 				ADMUX = _BV(ADLAR)|IN_PROT_TEMP_ADMUX;
 			else {
 				ADMUX = _BV(ADLAR)|IN_PWRFAIL_MUTE_ADMUX;
-				if(state == STATE_START)
+				if(state == STATE_START) {
 					/* first pass is done and we've checked both inputs so it's safe to transition to normal */
 					state = STATE_NORMAL;
+					delayvar = 0;
+				}
 			}
 
 			/* start new conversion */
@@ -128,28 +149,59 @@ int main(void) {
 			case STATE_START:
 				break;
 			case STATE_MUTE:
+				PORTB &= ~_BV(RELAY);
 				/* yellow */
 				PORTB |= _BV(LED_RED)|_BV(LED_GREEN);
 				break;
 			case STATE_PWRFAIL:
+				PORTB &= ~_BV(RELAY);
 				/* red */
 				PORTB |= _BV(LED_RED);
 				PORTB &= ~_BV(LED_GREEN);
 				break;
 			case STATE_TEMP:
-				/* TODO long blink red */
-				PORTB |= _BV(LED_RED);
+				PORTB &= ~_BV(RELAY);
 				PORTB &= ~_BV(LED_GREEN);
+
+				/* long blink red */
+				if(delayvar == 0)
+					PORTB |= _BV(LED_RED);
+				else if(delayvar == MEDIUM_DELAY)
+					PORTB &= ~_BV(LED_RED);
+
+				if(PORTB & _BV(LED_RED))
+					delayvar++;
+				else
+					delayvar--;
+
 				break;
 			case STATE_PROT:
-				/* TODO short blink red */
-				PORTB |= _BV(LED_RED);
+				PORTB &= ~_BV(RELAY);
 				PORTB &= ~_BV(LED_GREEN);
+
+				/* short blink red */
+				if(delayvar == 0)
+					PORTB |= _BV(LED_RED);
+				else if(delayvar == MEDIUM_DELAY/2)
+					PORTB &= ~_BV(LED_RED);
+
+				if(PORTB & _BV(LED_RED))
+					delayvar++;
+				else
+					delayvar--;
+
 				break;
 			case STATE_NORMAL:
-				/* green */
-				PORTB |= _BV(LED_GREEN);
-				PORTB &= ~_BV(LED_RED);
+				/* delay transitioning into relay on */
+				if(delayvar == MEDIUM_DELAY-1) {
+					PORTB |= _BV(RELAY);
+					/* green */
+					PORTB |= _BV(LED_GREEN);
+					PORTB &= ~_BV(LED_RED);
+					delayvar++;
+				}
+				else
+					delayvar++;
 				break;
 		}
 	}
